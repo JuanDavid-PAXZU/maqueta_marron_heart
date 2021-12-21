56a0ea402720d115c119b6f678a4a269910f5b4a
Juan David <juan.reyes@paxzu.co>
2021-12-21 16:21:19 -0500

    avance home, pendiente hero

--- js/functions.js
+++ js/functions.js
@@ -7,4 +7,30 @@ function swiperHomeHero(){
     });
 }
 
+function swiperNoticias(){
+  let swiper = new Swiper(".swiperNoticias", {
+    slidesPerView: 3,
+    spaceBetween: 0,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+}
+
+function swiperProductos(){
+  let swiper = new Swiper(".swiperProductos", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+}
+
 swiperHomeHero();
+swiperProductos();
+swiperNoticias();
